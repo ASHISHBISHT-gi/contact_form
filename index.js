9b79b4a992c18b1208d410515989f6c914c2b569
ASHISHBISHT-gi <syntanalprime@gmail.com>
2025-10-21 16:44:57 +0530

    mobile_dev_1.4_added_JS

---
+++ index.js
@@ -0,0 +1,30 @@
+const button=document.querySelector("button");
+const inputs=document.querySelectorAll('input:not([type="radio"])');
+const data=[inputs,document.querySelector("textarea"),document.querySelectorAll('input[type="radio"]')]
+const p=document.querySelectorAll("p");
+p.forEach(elements=>{
+    elements.style.display="none";
+})
+inputs.forEach(input => {
+    console.log("input's are ",input)
+});
+
+button.addEventListener('click',(e)=>{
+    data.forEach(set=>{
+        if(set.isArray()){
+            set.forEach((element,index)=>{
+                if(element.value=""){
+                    p[index].style.display="block";
+                }
+            })
+        }else{
+            if(set.value=""){
+                
+            }
+        }
+    }
+
+    )
+
+}
+)
