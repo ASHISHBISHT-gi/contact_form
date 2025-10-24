7eb37b0183517693e3075f4bb819abf865be5b8e
ASHISHBISHT-gi <syntanalprime@gmail.com>
2025-10-24 09:47:12 +0530

    mobile_dev_1.8

--- index.js
+++ index.js
@@ -11,38 +11,42 @@ p.forEach(elements=>{
     elements.style.display="none";
 })
 inputs.forEach(input => {
-    console.log({input})
+    // console.log({input})
 });
-let counter=[false,false,false,false];
+let counter=[false,false,false,false,false,false];
 button.addEventListener('click',(e)=>{
     e.preventDefault();
     data.forEach((set,i)=>{
         // for first three inputs
         if(set instanceof NodeList && i==0){
             set.forEach((element,index)=>{
-                console.log(element);
-                console.log("index is",index);
+                // console.log(element);
+                // console.log("index is",index);
                 // for first three inputs
                 if(element.value=="" && element.type!="checkbox"){
                     console.log("index is ",index);
                     p_new[index].style.display="block";
                     console.log(p_new[index].style.display);
-                    console.log("hello from 1st if");
-                    counter[0]=false;
+                    console.log("type from if is",element.type);
+                    // console.log("hello from 1st if");
+                    counter[index]=false;
                 }else{
-                    counter[0]=true;
-                    p_new[index].style.display="none";
-                    console.log("hello from 1st else");
+                    if(element.type!="checkbox"){
+                        counter[index]=true;
+                        console.log("type from else is",element.type);
+                        p_new[index].style.display="none";
+                        // console.log("hello from 1st else");
+                    }
                 }
                 // for checkbox
                 if(element.type=="checkbox" && element.checked==false){
                     p_new[3].style.display="block";
                     counter[3]=false;
-                    console.log("hello from 2nd if");
+                    // console.log("hello from 2nd if");
                 }else{
                     counter[3]=true;
                     p_new[3].style.display="none";
-                    console.log("hello from 2nd else");
+                    // console.log("hello from 2nd else");
                 }
             })
         } 
@@ -50,18 +54,18 @@ button.addEventListener('click',(e)=>{
         if(i==1){
             if(set.value==""){
                 p_new[4].style.display="block";
-                counter[1]=false;
+                counter[4]=false;
             }else{
-                counter[1]=true;
+                counter[4]=true;
                 p_new[4].style.display="none";
             }
         }else{
             // radio button validation logic
             if(set[0].checked || set[1].checked){
-                counter[2]=true;
+                counter[5]=true;
                 p_new[5].style.display="none";
             }else{
-                counter[2]=false;
+                counter[5]=false;
                 p_new[5].style.display="block";
             }
         }
@@ -74,7 +78,7 @@ button.addEventListener('click',(e)=>{
 
     )
     console.log("counter is",counter);
-    if(counter[0]==true && counter[1]==true && counter[2]==true){
+    if(counter[0]==true && counter[1]==true && counter[2]==true && counter[3]==true && counter[4]==true && counter[5]==true){
         const form = document.querySelector('form');
         form.submit();
         console.log("form submitted successfuly");
